abba42d015955a87c923eccd497213d35c448512
nils <nils@thepeople.de>
2016-01-10 18:54:09 +0100

    initial

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config['title'] = '';
-$config['description'] = '';
-
-$config['relpath'][0] = '/';
-$config['relpath'][1] = '/lowb1rd/www';
-$config['host'][0] = 'lowb1rd.github.io';
-$config['host'][1] = '192.168.99.201';
